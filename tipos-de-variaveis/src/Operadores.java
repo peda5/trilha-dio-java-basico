a3d9d3df35db14da0d7bb8cd84bf22c13d731dc8
peda5 <pedrokiagojunior23@gmail.com>
2024-12-31 18:58:32 -0300

    Operador Ternário

--- tipos-de-variaveis/src/Operadores.java
+++ tipos-de-variaveis/src/Operadores.java
@@ -1,12 +1,18 @@
 public class Operadores {
     public static void main(String[] args) {
-       boolean numero = false;
-       
-       System.out.println(!numero);
+  int a,b;
+  a = 6;
+  b = 6;
+//seguindo nessa ordem, 1 vem a condição verdadeira e depois dos ":" vem a condição falsa-
+//- condição verdadeira: condição falsa.
+  String resultado = a==b? "Verdadeiro" : "Falso";
+  
+  System.out.println(resultado);
 
-       boolean texto = true;
-       texto = !texto;
-       System.out.println(texto);
+  int resultado1 = a==b? 1:0;
+System.out.println(resultado1);
+
+  
         
     }
 }
