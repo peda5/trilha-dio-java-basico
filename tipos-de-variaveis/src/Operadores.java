1ec1c1e64714844c381042f53b54962cfa2efbd9
peda5 <pedrokiagojunior23@gmail.com>
2024-12-31 20:46:11 -0300

    Condição de "AND" e "OR"

--- tipos-de-variaveis/src/Operadores.java
+++ tipos-de-variaveis/src/Operadores.java
@@ -1,34 +1,23 @@
 public class Operadores {
     public static void main(String[] args) {
-        //Utilizamos o "equals" nesse caso, para sabermos se o conteúdo(o valor dado a variavel e a nova classe)-
-        //- conferem um com outro;
-        String nome = "Pedro";
-        String nome2 = new String("Pedro");
-       
-        System.out.println(nome.equals(nome2));
-  int numero1 = 1;
-  int numero2 = 2;
-  boolean simNao = numero1 == numero2;
-  
-   System.out.println(" Minha condição é: " + simNao);
-   String resultado = "";
-   if(numero1 < numero2)
-   resultado = "Verdadeiro";
-   System.out.println(resultado);
-
-    numero1 = 1;
-    numero2 = 2;
-    simNao = numero1 != numero2;
-    System.out.println(" O numeroUm é diferente ao numeroDois? " + simNao);
-    
-    numero1 = 1;
-    numero2 = 2;
-    simNao = numero1 < numero2;
-    System.out.println(" O numeroUm é menor que o numeroDois? " + simNao);
-    
-
-
-  
+    boolean condicao1 = true;
+    boolean condicao2 = true;
+    if(condicao1 && condicao2){
         
+        System.out.println("As duas condições são verdadeiras.");
+
+}
+
+if(condicao1 || condicao2 ){
+    System.out.println("Uma das condições é verdadeira.");
+}
+    
+
+    System.out.println("fim");
+    if(condicao1 && (7>3)){
+        System.out.println("As duas condições são verdadeiras" );
+
+    }
+
     }
 }
