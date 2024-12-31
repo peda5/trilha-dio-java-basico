7420be2818d6c4917da3366da6f1402325ddface
peda5 <pedrokiagojunior23@gmail.com>
2024-12-31 18:23:18 -0300

    Incremento e decremento.

--- tipos-de-variaveis/src/Operadores.java
+++ tipos-de-variaveis/src/Operadores.java
@@ -1,11 +1,15 @@
 public class Operadores {
     public static void main(String[] args) {
-  int numero = 5;
-  numero = - numero;
-  System.err.println(numero);
-  //Para transformar meu número em positivo mais uma vez nesse caso, multiplicamos a variavel por -1:
-  numero = numero* -1;
-  System.err.println(numero);
+       int numero = 5;
+       numero++;
+
+       System.out.println(++numero);
+    
+       numero = 5;
+       numero--;
+       
+       System.out.println(--numero);
+
         
     }
 }
